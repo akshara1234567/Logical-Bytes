2d5c96b0d0e7e40ceb881c26f775cccb5195da99
akshara1234567 <111179227+akshara1234567@users.noreply.github.com>
2022-08-13 19:41:48 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-alice = int(input("Enter alice's height"))
-bob = int(input("Enter bob's height"))
+def height():
+	trys = int(input("enter number of tries"))
+	if trys > 1000:
+		print("ERROR,ERROR. TRY AGAIN LATER!!")
+	elif trys < 1:
+		print("ERROR,ERROR. TRY AGAIN LATER!!")
+	else:
+		for i in range (trys):
+			alice = int(input("Enter Alice's height"))
+			if alice < 100:
+				print("ERROR,ERROR. TRY AGAIN LATER!!")
+				break
+			elif alice > 200:
+				print("ERROR,ERROR. TRY AGAIN LATER!!")
+				break
+			else:
+				bob = int(input("Enter Bob's height"))
+			if bob < 100:
+					print("ERROR,ERROR. TRY AGAIN LATER!!")
+					break
+			elif  bob > 200:
+				print("ERROR,ERROR. TRY AGAIN LATER!!")
+				break
+			elif bob == alice:
+				print("ERROR,ERROR. TRY AGAIN LATER!!")
+				break
+			else:
+				if alice > bob:
+  						print("A")
 
-if alice > bob:
-    print("A")
+				else:
+						bob > alice
+						print("B")
 
-elif bob > alice:
-    print("B")
-
-else:
-    bob = alice
-    print("ERROR,ERROR. TRY AGAIN LATER!!")
+height()
