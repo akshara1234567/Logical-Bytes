a97642fbbd6263455d25559aec638ff6e8d2ce92
akshara1234567 <111179227+akshara1234567@users.noreply.github.com>
2022-08-13 18:31:11 +0800

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,30 @@
+print("Hello! WELCOME TO QUIZ. Answer the questions in the shortest manner to see how well you know climate change")
+question1 = input(" why does climate change need serious action? Enter answer in minimum words - ")
+
+if question1 == ( "to protect earth"):
+    print("CORRECT!!")
+else:
+    print("don't give up")
+question2 = input("What happens when smoke hits the sky? Enter answer in minimum words - ")
+if question2 == ("global warming" ):
+    print("CORRECT!!")
+else:
+    print("don't give up")
+question3 = input(" what layer is affected by climate change? Enter answer in minimum words - ")
+if question3 == (" ozone layer"):
+    print("CORRECT!!")
+else:
+    print("don't give up")
+question4 = input(" what do trees do in climate change? Enter answer in minimum words - ")
+if question4 == ("clean the air"):
+    print("CORRECT!!")
+else:
+    print("don't give up")
+question5 =  input(" true or false- it is  good to do community services. Enter answer in minimum words - ")
+if question5 == ("true"):
+    print("CORRECT!!")
+else:
+    print("don't give up")
+question6 = input("rate this from 1- 10 :")
+
+print("THANK YOU FOR PLAYING")
