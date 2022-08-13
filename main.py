668f01670eb67a858cc0dad6e6df8307ae1e48a6
akshara1234567 <111179227+akshara1234567@users.noreply.github.com>
2022-08-13 20:18:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
-def height():
-	trys = int(input("enter number of tries"))
-	if trys > 1000:
-		print("ERROR,ERROR. TRY AGAIN LATER!!")
-	elif trys < 1:
-		print("ERROR,ERROR. TRY AGAIN LATER!!")
-	else:
-		for i in range (trys):
-			alice = int(input("Enter Alice's height"))
-			if alice < 100:
-				print("ERROR,ERROR. TRY AGAIN LATER!!")
-				break
-			elif alice > 200:
-				print("ERROR,ERROR. TRY AGAIN LATER!!")
-				break
-			else:
-				bob = int(input("Enter Bob's height"))
-			if bob < 100:
-					print("ERROR,ERROR. TRY AGAIN LATER!!")
-					break
-			elif  bob > 200:
-				print("ERROR,ERROR. TRY AGAIN LATER!!")
-				break
-			elif bob == alice:
-				print("ERROR,ERROR. TRY AGAIN LATER!!")
-				break
-			else:
-				if alice > bob:
-  						print("A")
+trys = int(input("enter number of tries : "))
 
-				else:
-						bob > alice
-						print("B")
 
-height()
+if trys > 1000:
+		print("ERROR,ERROR. TRY AGAIN LATER!!")
+
+elif trys < 1:
+	print("ERROR,ERROR. TRY AGAIN LATER!!")
+
+else:
+    for i in range (trys):
+        X = int(input("enter amount to pay :"))
+
+        if X > 1000:
+             print("ERROR,ERROR. TRY AGAIN LATER!!")
+
+        elif X < 1:
+            print("ERROR,ERROR. TRY AGAIN LATER!!")
+
+        else:
+            coins = X%10
+            print("You have to pay: ",coins,"coins")
+            total = X//10
+            print("TRY NUMBER",i + 1,":: You have to pay: ",total,"rupee notes and",coins,"coins")
+
